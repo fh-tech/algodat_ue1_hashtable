4c35b03a10dcf5b8be382febc30d9ff672d17818
Daniel Kudernatsch <d.kudernatsch@outlook.com>
2018-03-15 15:32:56 +0100

    removed cmake build directory from source control

--- cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/Nextcloud/Programming/Cpp/ue1_hashtable/lib/googletest-master
-# Build directory: /home/daniel/Nextcloud/Programming/Cpp/ue1_hashtable/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
